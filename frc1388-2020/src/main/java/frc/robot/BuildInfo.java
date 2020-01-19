14f39bbce3106f496ce455a761082ab4cadef16d
HaydenUndesser17 <hayden.undesser@gmail.com>
2020-01-18 16:03:16 -0800

    Arm Motor and Spinner Motor created with speed.

---
+++ frc1388-2020/src/main/java/frc/robot/BuildInfo.java
@@ -0,0 +1,8 @@
+package frc.robot;
+public class BuildInfo {
+  public static final String GIT_VERSION = "3ea2302";
+  public static final String GIT_BRANCH = "master";
+  public static final String GIT_STATUS = "";
+  public static final String BUILD_DATE = "Sat 01/18/2020 ";
+  public static final String BUILD_TIME = "01:49 PM";
+}
